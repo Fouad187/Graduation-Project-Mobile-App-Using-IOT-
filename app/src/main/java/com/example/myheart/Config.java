7b7168924a855e608f3a3e68e4e89a63ffe883ad
unknown <fouadabdelaziz1998@gmail.com>
2020-12-01 15:44:27 +0200

    second

--- app/src/main/java/com/example/myheart/Config.java
+++ app/src/main/java/com/example/myheart/Config.java
@@ -1,6 +1,6 @@
 package com.example.myheart;
 
 public class Config {
-    public static final String EMAIL ="myheartapplication2020@gmail.com"; //your-gmail-username
-    public static final String PASSWORD ="01017036414"; //your-gmail-password
+    public static final String EMAIL =""; //your-gmail-username
+    public static final String PASSWORD =""; //your-gmail-password
 }
